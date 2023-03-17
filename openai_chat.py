49b376c85b2b4d5e52b58f4d1098f983d3b303fe
netriser <89742893+netriser@users.noreply.github.com>
2023-03-17 21:28:07 +0100

    Create openai_chat.py

---
+++ openai_chat.py
@@ -0,0 +1,33 @@
+import openai
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
+from colorama import just_fix_windows_console
+just_fix_windows_console()
+
+# Define OpenAI API key 
+openai.api_key = "API_KEY"
+
+history = []
+while True:
+    user_input = input("Q: " + Style.BRIGHT + Fore.RED)
+    print(Style.RESET_ALL)
+    messages = []
+    for input_text, completion_text in history:
+        messages.append({"role": "user", "content": input_text})
+        messages.append({"role": "assistant", "content": completion_text})
+
+    messages.append({"role": "user", "content": user_input})
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    completion_text = completion.choices[0].message.content
+    print(Style.BRIGHT + Fore.GREEN + completion_text + Style.RESET_ALL)
+
+    history.append((user_input, completion_text))
+
+    if user_input.upper() == "QUIT":
+        break
